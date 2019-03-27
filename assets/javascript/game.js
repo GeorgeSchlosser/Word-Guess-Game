526922b01ee46d05628c81c3ed3df1e5ff96e8c8
Trash Panda <d.george.schlosser@gmail.com>
2019-03-27 02:49:40 -0400

    html linked w/ js added alert

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,34 @@
 
     //Press any key to get started!
-    // onclick.start script
+    // onclick.start script;
 
-    // "We have such sights to show you"
+    alert("We have such sights to show you");
+    console.log(alert);
 
-    // div for each
-    var wins;
-    var guesses;
-    // 
+    // div for wins, guesses remaining,
+    var wins = 0;
+    // var guesses = wordbank[x].length;
     var lives;
+    // case sensitivity?
+    var wordbank = ["Freddy","Jason", "redrum"];
+    // var playerGuess = keyInput;
     var lettersGuessed;
-    // function to print lettersGuessed
+    // function to print lettersGuessed;
 
 
-    {/* Display Blank Characters of Current Word FUNCTION */}
+    // Display Blank Characters of Current Word FUNCTION 
 
-    {/* Number of Guesses Remaining: xx  */}
+    // Number of Guesses Remaining: xx  
+
+    //  if playerGuess === possibleWord {
+        // reveal letter;
+    // }
+    // else if playerGuess !=== possibleWord {
+        // guess--;
+    // }
+    // if guesses ==== 0 {
+    //     lives--;
+    // }
+    // if lives === 0 {
+    //     alert("GAME OVER");
+    // }
