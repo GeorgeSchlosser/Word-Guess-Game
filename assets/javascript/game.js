189996a60401401713142cd7f56e55f906b7d63c
Trash Panda <d.george.schlosser@gmail.com>
2019-03-26 23:44:18 -0400

    minor additions

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+    //Press any key to get started!
+    // onclick.start script
+
+    // "We have such sights to show you"
+
+    // div for each
+    var wins;
+    var guesses;
+    // 
+    var lives;
+    var lettersGuessed;
+    // function to print lettersGuessed
+
+
+    {/* Display Blank Characters of Current Word FUNCTION */}
+
+    {/* Number of Guesses Remaining: xx  */}
