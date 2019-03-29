16840f78d4ca73e7d7000a551fe115f5ea9cdac6
Trash Panda <d.george.schlosser@gmail.com>
2019-03-29 02:15:17 -0400

    blank generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var wordbank = ["freddy","jason", "redrum", "xenomorph", "cenobite", "necronomic
 var currentWord = "";
 var wordLetters = [];
 var blanks = 0;
-var blanksGuesses = [];
+var blanksGuesses = []; //e _ _ _ _ _
 var wrongGuesses = [];
 
 // Counters
@@ -13,8 +13,8 @@ var wins = 0;
 var guesses = 6;
 
 // Tests
-console.log(wordbank);
-console.log(guesses);
+// console.log(wordbank);
+// console.log(guesses);
 
 
 //FUNCTIONS
@@ -25,10 +25,22 @@ function startGame () {
     wordLetters = currentWord.split("");
     blanks = wordLetters.length;
 
+    // Resets
+    guesses = 6;
+    wrongGuesses = [];
+    blanksGuesses = [];
+
+    // populate blanks and successes w/ correct # of blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksGuesses.push("_");
+        
+    }
+
     // Testing
     console.log(currentWord);
     console.log(wordLetters);
     console.log(blanks);
+    console.log(blanksGuesses); //WOOT!
 }
 
 //MAIN PROCESS(Call Functions To Do Things)
